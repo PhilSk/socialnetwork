041a3137b9d080eb6207f4903184c97cbf865ab8
ZZZZZZZZZZZZZ <parrot-stuido@hotmail.com>
2016-10-01 00:21:42 +0300

    Added Chat and Friendship

--- src/friendship/models.py
+++ src/friendship/models.py
@@ -1,28 +1,34 @@
+# coding=utf-8
 from __future__ import unicode_literals
 
 from django.conf import settings
 from django.db import models
+from enum import Enum
 
 
 # Create your models here.
+class Statements(Enum):
+    accepted = True
+    declined = False
+
 
 class Friendship(models.Model):
     """
     Илья, посмотри, как нам лучше сделать?
     Тут нас что-то несколько смущает, может быть мы не увидели более очевидного решения?
     На данный момент состояние заявки в друзья определяется так:
-    accepted_first accepted_second
-           0              0       (отсутствие записи - не друзья)
-           1              0       (первый отправил, второй не подтвердил)
-           0              1       (второй отправил, первый не ответил)
-           1              1       (друзья)
+    sender_state receiver_state
+        0              0       (отсутствие записи - не друзья)
+        1              0       (первый отправил, второй не подтвердил)
+        0              1       (второй отправил, первый не ответил)
+        1              1       (друзья)
     """
-    id1 = models.ForeignKey(settings.AUTH_USER_MODEL)
-    id2 = models.ForeignKey(settings.AUTH_USER_MODEL)
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='sender')
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='receiver')
 
-    accepted_first = models.BooleanField(
-        default=False
+    sender_state = models.BooleanField(
+        default=Statements.declined
     )
-    accepted_second = models.BooleanField(
-        default=False
+    receiver_state = models.BooleanField(
+        default=Statements.declined
     )
