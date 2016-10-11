88cea8cad0438c4afe8603954414f2a4b8135988
Dmitriy <parrot-studio@hotmail.com>
2016-10-11 14:03:19 +0400

    Delete local_settings.py
    Не знаю как, но почему-то он сюда отправился, хотя не должен был

--- src/application/local_settings.py
+++
@@ -1,9 +0,0 @@
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = '/home/dmitriy/MyEnv/DjangoProjects/socialnetwork/collected_static/'
-STATICFILES_DIRS = ('/home/dmitriy/MyEnv/DjangoProjects/socialnetwork/src/static/',)
-
-# All media files such as avatar will be here
-MEDIA_ROOT = '/home/dmitriy/MyEnv/DjangoProjects/socialnetwork/media/'
