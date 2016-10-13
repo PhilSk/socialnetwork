810a73e68ce04ca78f55b42e308d80b49a208ee3
ZZZZZZZZZZZZZ <parrot-stuido@hotmail.com>
2016-10-13 20:29:57 +0300

    added requirements

--- src/application/settings.py
+++ src/application/settings.py
@@ -135,7 +135,15 @@ USE_L10N = True
 USE_TZ = True
 
 
-import local_settings
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-f..
+
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/dmitriy/MyEnv/DjangoProjects/socialnetwork/collected_static/'
+STATICFILES_DIRS = ('/home/dmitriy/MyEnv/DjangoProjects/socialnetwork/src/static/',)
+
+# All media files such as avatar will be here
+MEDIA_ROOT = '/home/dmitriy/MyEnv/DjangoProjects/socialnetwork/media/'
 
 # My user model
 AUTH_USER_MODEL = 'extuser.ExtUser'
